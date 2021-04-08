e1a3599c9868875ac877dce4084c917686be985a
Taylor Niles <tniles320@gmail.com>
2021-04-08 10:13:56 -0700

    comment out unnecessary code

--- client/src/pages/AdminLogin/index.js
+++ client/src/pages/AdminLogin/index.js
@@ -10,30 +10,30 @@ function AdminLogin(props) {
     password: "",
   });
 
-  const userData = { username: "janinefitness@msn.com", password: "password" };
-  const register = () => {
-    console.log(userData);
-    ADMIN.register(userData).then((res) => {
-      console.log(res);
-    });
-    API.addFeatured({
-      headline: "Test",
-      description: "Test",
-      link: "",
-      image: "1617072507655-Over-Project1.png",
-    }).then((res) => {
-      if (res.status === 422) {
-        console.log(res);
-        alert("Error adding content");
-      } else {
-        alert("Featured Content added successfully!");
-      }
-    });
-  };
+  // const userData = { username: "janinefitness@msn.com", password: "password" };
+  // const register = () => {
+  //   console.log(userData);
+  //   ADMIN.register(userData).then((res) => {
+  //     console.log(res);
+  //   });
+  //   API.addFeatured({
+  //     headline: "Test",
+  //     description: "Test",
+  //     link: "",
+  //     image: "1617072507655-Over-Project1.png",
+  //   }).then((res) => {
+  //     if (res.status === 422) {
+  //       console.log(res);
+  //       alert("Error adding content");
+  //     } else {
+  //       alert("Featured Content added successfully!");
+  //     }
+  //   });
+  // };
 
-  useEffect(() => {
-    register();
-  }, []);
+  // useEffect(() => {
+  //   register();
+  // }, []);
 
   return (
     <div className="loginContainer">
