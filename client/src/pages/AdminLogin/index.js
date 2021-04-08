a021887c26550392bdf610e3ac8b605ec4f6fd49
Taylor Niles <tniles320@gmail.com>
2021-04-08 10:09:21 -0700

    heroku build

--- client/src/pages/AdminLogin/index.js
+++ client/src/pages/AdminLogin/index.js
@@ -1,5 +1,6 @@
 import { useState, useContext, useEffect } from "react";
 import ADMIN from "../../utils/ADMIN";
+import API from "../../utils/API";
 import "./style.css";
 
 function AdminLogin(props) {
@@ -9,11 +10,24 @@ function AdminLogin(props) {
     password: "",
   });
 
-  const userData = { username: "tniles320@gmail.com", password: "password" };
+  const userData = { username: "janinefitness@msn.com", password: "password" };
   const register = () => {
     console.log(userData);
     ADMIN.register(userData).then((res) => {
       console.log(res);
+    });
+    API.addFeatured({
+      headline: "Test",
+      description: "Test",
+      link: "",
+      image: "1617072507655-Over-Project1.png",
+    }).then((res) => {
+      if (res.status === 422) {
+        console.log(res);
+        alert("Error adding content");
+      } else {
+        alert("Featured Content added successfully!");
+      }
     });
   };
 
@@ -30,7 +44,9 @@ function AdminLogin(props) {
         <input
           type="email"
           placeholder="Email"
-          onChange={(e) => setUser({ ...user, username: e.target.value })}
+          onChange={(e) =>
+            setUser({ ...user, username: e.target.value.toLowerCase() })
+          }
         ></input>
         <input
           type="password"
