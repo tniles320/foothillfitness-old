da5abe5bf2684b5a3579863863916a6122159e30
Taylor Niles <tniles320@gmail.com>
2021-04-08 15:43:18 -0700

    updating for heroku

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(
   cors({
-    origin: "http://localhost:3000", // <-- location of the react app were connecting to
+    // origin: "http://localhost:3000", // <-- location of the react app were connecting to
     credentials: true,
   })
 );
