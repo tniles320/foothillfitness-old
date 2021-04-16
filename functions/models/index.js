72c90de187fae0643569b07b900eca43c28824c3
Taylor Niles <tniles320@gmail.com>
2021-04-16 08:09:43 -0700

    update

--- functions/models/index.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  User: require("./User"),
-  Strength: require("./Strength"),
-  Cardio: require("./Cardio"),
-  Brand: require("./Brand"),
-  Image: require("./Image"),
-  Featured: require("./Featured"),
-};
